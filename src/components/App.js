8c5b1647621d353294ab796737ae26f73f03a781
sherry <sherry.kisilu@studebnt.moringaschool.com>
2023-01-12 18:24:53 +0300

    done

--- src/components/App.js
+++ src/components/App.js
@@ -3,8 +3,6 @@ import MovieCard from "./MovieCard";
 
 function App() {
   const title = "Mad Max";
-  const posterURL =
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTn1OTYGz2GDC1XjA9tirh_1Rd571yE5UFIYsmZp4nACMd7CCHM";
   const genresArr = ["Action", "Adventure", "Science Fiction", "Thriller"];
 
   return (
@@ -13,5 +11,6 @@ function App() {
     </div>
   );
 }
+
 
 export default App;
